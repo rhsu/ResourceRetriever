c9d221405074578a11d6919796398296bb066fe0
rhsu <rhsu123@gmail.com>
2014-07-02 13:56:16 -0400

    version 1

--- src/rhsu/resourceRetriever/ResourceRetriever.java
+++ src/rhsu/resourceRetriever/ResourceRetriever.java
@@ -4,6 +4,5 @@ import java.io.BufferedReader;
 
 public interface ResourceRetriever
 {
-	public BufferedReader getResource(String filename);
-	public BufferedReader getResource(String filename, String location);
+	public BufferedReader getReader();
 }
--- src/rhsu/resourceRetriever/ResourceRetrieverImpl.java
+++ src/rhsu/resourceRetriever/ResourceRetrieverImpl.java
@@ -7,26 +7,16 @@ public class ResourceRetrieverImpl implements ResourceRetriever
 {
 	private final BufferedReader bufferedReader;
 	
-	private ResourceRetrieverImpl(String filename, String location)
+	public ResourceRetrieverImpl(String filename, String location)
 	{
 		bufferedReader = new BufferedReader(new InputStreamReader( getClass().getResourceAsStream(location + filename)));
 	}
 	
 	@Override
-	public BufferedReader getResource(String filename)
-	{
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public BufferedReader getResource(String filename, String location)
-	{
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-	
+	public BufferedReader getReader() { return this.bufferedReader; }
+		
 	public static ResourceRetriever getResourceRetriever(String filename, String location)
 	{
 		return new ResourceRetrieverImpl(filename, location);
-	}
-	
+	}	
 }
